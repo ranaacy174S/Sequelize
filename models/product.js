f865712dcbb706e3204679723a222e2ab13fc5ae
ranaacy174S <sunilrana1730@gmail.com>
2022-08-18 00:25:04 +0530

    dynamic route

---
+++ models/product.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(id,title, imageUrl, description, price) {
+    this.id=id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    getProductsFromFile(products => {
+      if(this.id){
+        let existingProductIndex=products.findIndex(prod=>prod.id===this.id);
+        let updatedProduct=this;
+        products[existingProductIndex]=updatedProduct;
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
+      else{
+        this.id=Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+      }
+      
+    });
+  }
+
+  static deleteProductById(id){
+    getProductsFromFile(products=>{
+      const existingProductIndex=products.findIndex(prod=>prod.id===id)
+      const updatedProducts=products.filter((prod,index)=>{
+        if(index!==existingProductIndex)
+        return prod;
+      })
+      fs.writeFile(p,JSON.stringify(updatedProducts),err=>{
+        console.log(err)
+      })
+    })
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id,cb){
+    getProductsFromFile(products=>{
+      const product=products.find(p=>p.id===id);
+      cb(product);
+    })
+  }
+};
